af06013e2987f20eb0bd805943625438ebadfee0
Andrew <rolyata47@gmail.com>
2021-07-31 22:01:40 -0500

    First commit

---
+++ src/randomwalk/RandomWalk.java
@@ -0,0 +1,17 @@
+package randomwalk;
+
+/**
+ *
+ * @author andrewtaylor
+ */
+public class RandomWalk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        RandomWalkJFrame window = new RandomWalkJFrame();
+        window.setVisible(true);
+    }
+    
+}
